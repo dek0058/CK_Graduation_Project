e8e9f5c740ea847c0982ac34b947c969c0b457b0
cmd3689 <47653276+dek0058@users.noreply.github.com>
2020-03-23 15:14:33 +0900

    Update7.1
    흑백 필터 추가

---
+++ Assets/Scripts/RARARA/TestGrayCam.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class TestGrayCam : JToolkit.Utility.Singleton<TestGrayCam>
+{
+    [HideInInspector] public RenderTexture render;
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+        if (cam == null)
+            Destroy(this);
+
+        if (cam.targetTexture != null)
+            cam.targetTexture.Release();
+
+        render = new RenderTexture(Screen.width, Screen.height, 24);
+        cam.targetTexture = render;
+    }
+}
---
+++ Assets/Scripts/RARARA/TestGrayImage.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TestGrayImage : MonoBehaviour
+{
+    public GameObject start_obj;
+    private RawImage image;
+
+    private void Start()
+    {
+        image = GetComponent<RawImage>();
+        if (image == null)
+            Destroy(this);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (image.texture == null)
+                image.texture = TestGrayCam.instance.render;
+
+            if (start_obj != null)
+                start_obj.SetActive(!start_obj.activeSelf);
+
+            image.enabled = !image.enabled;
+        }
+    }
+}
