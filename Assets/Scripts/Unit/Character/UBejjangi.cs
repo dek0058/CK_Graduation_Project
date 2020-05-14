39d6c35d314dacbe0885745ae33b92b5056bb189
cmd3689 <47653276+dek0058@users.noreply.github.com>
2020-05-14 21:17:25 +0900

    #Update 8.2.3
    스테이지1 벽, 천장, 바닥 리소스 추가

---
+++ Assets/Scripts/Unit/Character/UBejjangi.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace Game.Unit.Character
+{
+    using Type;
+
+    public class UBejjangi : UUnit
+    {
+        private enum ActionId
+        {
+            Idle = 0,
+            Movement = 1,
+        }
+        
+        private BejjangiType my_type;
+        
+        public override void confirm()
+        {
+            base.confirm();
+            
+            my_type = unit_type as BejjangiType;
+        }
+
+        protected override void active_rotate()
+        {
+            base.active_rotate();
+        }
+
+        protected override void active_move()
+        {
+            if (unit_status.input == Vector2.zero)
+            {
+                return;
+            }
+
+            base.active_move();
+            if (get_animator().GetInteger(state_para[AnimatorParameter.OrderId]) != (int)ActionId.Movement)
+            {
+                action_animation((int)ActionId.Movement);
+            }
+        }
+        
+        protected override void active_update()
+        {
+            base.active_update();
+        }
+
+        protected override void active_fixedupdate()
+        {
+            base.active_fixedupdate();
+        }
+
+        protected override void active_lateupdate()
+        {
+            base.active_lateupdate();
+        }
+    }
+}
